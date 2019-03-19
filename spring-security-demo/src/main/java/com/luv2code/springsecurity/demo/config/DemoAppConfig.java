e1121f8b652a1fab90d9d3e8016fe57514232656
karanbhalla <kbhalla22@gmail.com>
2019-03-19 16:46:04 -0400

    Added maven dependancies

--- spring-security-demo/src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
+++ spring-security-demo/src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
@@ -9,17 +9,29 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages="com.luv2code.springsecurity.demo.config")
+@ComponentScan(basePackages="com.luv2code.springsecurity.demo")
 public class DemoAppConfig {
-	
-	//define a bean for ViewResolver
+
+	// define a bean for ViewResolver
+
 	@Bean
 	public ViewResolver viewResolver() {
-		InternalResourceViewResolver viewResolver=new InternalResourceViewResolver();
+		
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		
 		viewResolver.setPrefix("/WEB-INF/view/");
 		viewResolver.setSuffix(".jsp");
+		
 		return viewResolver;
 	}
 	
-
 }
+
+
+
+
+
+
+
+
+
