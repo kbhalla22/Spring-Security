d437e88d7ecd967ba293b8a5eb2e91a7b181386a
karanbhalla <kbhalla22@gmail.com>
2019-03-23 12:38:36 -0400

    Added database authentication

--- spring-security-demo-08-jdbc-plaintext/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo-08-jdbc-plaintext/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.luv2code.springsecurity.demo.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,15 +14,15 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
-
+	@Autowired
+	private DataSource securityDataSource;
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-		//add our users for in memory authentication
 		
-		UserBuilder users=User.withDefaultPasswordEncoder();
+		//add a reference to our security data source
 		
-		auth.inMemoryAuthentication().withUser(users.username("john").password("test123").roles("EMPLOYEE"));
-		auth.inMemoryAuthentication().withUser(users.username("mary").password("test123").roles("EMPLOYEE","MANAGER"));
-		auth.inMemoryAuthentication().withUser(users.username("karan").password("test123").roles("EMPLOYEE"," ADMIN"));
+	//use jdbc authentication
+		
+		auth.jdbcAuthentication().dataSource(securityDataSource);
 		
 		
 }
